c7695b8f327d4480432360438184ad66bd2f524d
Mahlomola21 <mahlomolaseepe6@gmail.com>
2024-10-01 17:46:22 +0200

    Your commit message here

--- spam_detector/server.js
+++ spam_detector/server.js
@@ -8,6 +8,15 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
+
+// Serve static files from the "frontend" directory
+app.use(express.static(path.join(__dirname, 'spam_detector')));
+
+// Route for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'spam_detector', 'index.html')); // Serve the home.html file
+});
+
 app.post('/check-email', async (req, res) => {
     const { emailText } = req.body;
 
